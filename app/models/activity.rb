da9c24ed918c1c0d5a9785002340987df8668b23
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-16 17:20:44 +0530

    Resolve Bug: time not being saved properly

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,11 +1,14 @@
+include ActionView::Helpers::TextHelper
 class Activity < ActiveRecord::Base
 	belongs_to :user, dependent: :destroy
 
-	def total_time_spent
+	def total_time_spent_in_words
 		s_time = Time.parse(start_time.to_s)
 		e_time = Time.parse(end_time.to_s)
-		hours_spent = (e_time - s_time)/3600
-		hours_spent
+		total_spent = (e_time - s_time)
+		hours = (total_spent/3600).to_i
+		minutes = ((total_spent - (hours*3600))/60).to_i
+		"#{pluralize(hours, 'hour') + ' and' if hours!=0} #{pluralize(minutes, 'minute')}"
 	end
 
 	def friendly_created_at
