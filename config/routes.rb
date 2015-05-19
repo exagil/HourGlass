21d54ecc42450dee15651a1b47a375fd639ee64a
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-19 12:39:29 +0530

    Update the style for the welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}, controllers: { confirmations: 'confirmations' }
   resources :users, only: :destroy
   get 'welcome' => 'activities#edit'
   get 'dashboard' => 'activities#dashboard'
   resources :activities, only: [:update, :index]
   get 'history' => 'activities#index'
   root 'static_pages#home'
+  get 'one' => 'static_pages#one'
 end
